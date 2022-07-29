4fee9379b04a261bcd8b13011274e3ed983182d4
Angelo Rosso <angelo.rosso.19@cnu.edu>
2022-07-29 19:39:42 -0400

    implemented different board sizes and make the loss checker more efficient

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import pygame as pg
 import numpy as np
 
 class Box:
-    def __init__(self, pos, color, off=1):
-        self.pos = [pos[0], -100 * off]
+    def __init__(self, pos, color, size, off=1):
+        self.pos = [pos[0], - size[1] * off]
         self.color = color
         self.dest = pos
         self.falling = True
+        self.size = size
 
     def move(self):
-        if self.pos[1] != self.dest[1]:
+        if self.dest[1] - self.pos[1] >= 4:
             self.pos[1] += 4
         else:
+            self.pos[1] = self.dest[1]
             self.falling = False
 
     def draw(self, win):
-        pg.draw.rect(win, self.color, [self.pos[0] + 2, self.pos[1] + 2, 96, 96], border_radius=5)
+        pg.draw.rect(win, self.color, [self.pos[0] + 2, self.pos[1] + 2, self.size[0] - 4, self.size[1] - 4], border_radius=5)
 
 
 class App:
@@ -29,7 +31,7 @@ class App:
         self.clock = pg.time.Clock()
         self.win = pg.display.set_mode((self.screenWidth, self.screenHeight))
         pg.display.set_caption("Guess")
-        self.array = [[0 for _ in range(80)] for _ in range(60)]
+        # self.array = [[0 for _ in range(80)] for _ in range(60)]
         self.tab_screen = 0
 
     def check(self, board, coord, color, group=False):
@@ -43,7 +45,7 @@ class App:
                     for nay in nays:
                         if nay not in group:
                             group.append(nay)
-        if coord[0] < 7:
+        if coord[0] < (len(board) - 1):
             if board[coord[0] + 1][coord[1]] == color:
                 if [coord[0] + 1, coord[1]] not in group:
                     group.append([coord[0] + 1, coord[1]])
@@ -59,7 +61,7 @@ class App:
                     for nay in nays:
                         if nay not in group:
                             group.append(nay)
-        if coord[1] < 5:
+        if coord[1] < (len(board[0]) - 1):
             if board[coord[0]][coord[1] + 1] == color:
                 if [coord[0], coord[1] + 1] not in group:
                     group.append([coord[0], coord[1] + 1])
@@ -70,11 +72,13 @@ class App:
         return group
 
     def game(self):
-        colors = ["BLUE", "RED", "PURPLE", "YELLOW", "ORANGE", "GREEN"]
+        colors = ["BLUE", "RED", [30, 190, 30], "YELLOW", "ORANGE", "PURPLE"]
         font_big = pg.font.SysFont('comicsans', 80, True)
         font_small = pg.font.SysFont('comicsans', 25, True)
-        board = [[np.random.randint(0, 4) for _ in range(6)] for _ in range(8)]
-        boxes = [[Box([100 * i, 500 - 100 * j], colors[board[i][j]], j) for j in range(len(board[i]))] for i in range(len(board))]
+        board_size = [8, 6]
+        size = [int(self.screenWidth / board_size[0]), int(self.screenHeight / board_size[1])]
+        board = [[np.random.randint(0, 4) for _ in range(board_size[1])] for _ in range(board_size[0])]
+        boxes = [[Box([size[0] * i, (self.screenHeight - size[1]) - (size[1] * j)], colors[board[i][j]], size, j) for j in range(len(board[i]))] for i in range(len(board))]
         run = True
         score = 0
         lose = False
@@ -103,7 +107,7 @@ class App:
                 if event.type == pg.MOUSEBUTTONUP and not falling:
                     if event.button == 1:
                         pos = pg.mouse.get_pos()
-                        pos = [pos[0] // 100, 5 - pos[1] // 100]
+                        pos = [pos[0] // size[0], (board_size[1] - 1) - pos[1] // size[1]]
                         kills = self.check(board, pos, board[pos[0]][pos[1]])
                         if len(kills) > 2:
                             falling = True
@@ -121,7 +125,7 @@ class App:
                                 if boxes[i][j] is None:
                                     dif += 1
                                 else:
-                                    boxes[i][j].dest = [boxes[i][j].pos[0], boxes[i][j].pos[1] + dif * 100]
+                                    boxes[i][j].dest = [boxes[i][j].pos[0], boxes[i][j].pos[1] + dif * size[1]]
                                     boxes[i][j].falling = True
                         for col in boxes:
                             while None in col:
@@ -129,7 +133,7 @@ class App:
                         for i in range(len(board)):
                             col = board[i]
                             off = 1
-                            while len(col) < 6:
+                            while len(col) < board_size[1]:
                                 if score > 250:
                                     c_num = np.random.randint(0, 6)
                                 elif score > 100:
@@ -137,20 +141,23 @@ class App:
                                 else:
                                     c_num = np.random.randint(0, 4)
                                 col.append(c_num)
-                                boxes[i].append(Box([100 * i, 500 - 100 * (len(boxes[i]))], colors[c_num], off))
+                                boxes[i].append(Box([size[0] * i, (self.screenHeight - size[1]) - size[1] * (len(boxes[i]))], colors[c_num], size, off))
                                 off += 1
                     lose = True
-                    for i in range(8):
-                        for j in range(6):
-                            if len(self.check(board, [i, j], board[i][j])) > 2:
-                                lose = False
+                    all = [[[i, j], board[i][j]] for i in range(board_size[0]) for j in range(board_size[1])]
+                    for po in all:
+                        group = self.check(board, po[0], board[po[0][0]][po[0][1]])
+                        if len(group) > 2:
+                            lose = False
+                            break
+                        for p in group:
+                            all.remove([[p[0], p[1]], board[p[0]][p[1]]])
                     if lose:
                         print("YOU LOSE")
-
                 if event.type == pg.KEYUP:
                     if event.key == pg.K_r:
-                        board = [[np.random.randint(0, 4) for _ in range(6)] for _ in range(8)]
-                        boxes = [[Box([100 * i, 500 - 100 * j], colors[board[i][j]], j) for j in range(len(board[i]))] for i in range(len(board))]
+                        board = [[np.random.randint(0, 4) for _ in range(board_size[0])] for _ in range(board_size[1])]
+                        boxes = [[Box([size[0] * i, (self.screenHeight - size[1]) - (size[1] * j)], colors[board[i][j]],size, j) for j in range(len(board[i]))] for i in range(len(board))]
                         score = 0
                         lose = False
                 elif event.type == pg.QUIT:
